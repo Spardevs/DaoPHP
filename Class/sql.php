10fdd8bfb8085be04f48273e29d64ec99b78f9e1
Gabriel Fernandes Ramos <gabrielframos02@gmail.com>
2022-03-13 09:33:58 -0400

    Atualização

--- Class/sql.php
+++ Class/sql.php
@@ -3,17 +3,17 @@
     class Sql extends PDO{
         private $conn;
         public function __construct(){
-            $this->conn = new PDO("mysql:host-localhost;dbname=dbphp7", "root", "");
+            $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
         }
 
-        private function setParams($statment, $parameters = array()){
+        private function setParams($statement, $parameters = array()){
             foreach ($parameters as $key => $value) {
-                $statment->bindParam($key, $value);
+                $statement->setParam($statement, $key, $value);
             }
         }
 
-        private function setParam($statment, $key, $value){
-            $statment->bindParam($key, $value);
+        private function setParam($statement, $key, $value){
+            $statement->bindParam($key, $value);
         }
 
         public function execQuery($rawQuery, $params = array()){
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# DAO
+#  DAO & PDO em PHP
+
+Reporistório para a aplicação básica de Data Access Object e PDO no PHP7
  
