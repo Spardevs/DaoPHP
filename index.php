8b325602e2a9462f7a8d8c3146b096b7fa7b0786
Gabriel Fernandes Ramos <gabrielframos02@gmail.com>
2022-03-14 19:42:33 -0400

    Metodos de lista

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
     require_once("config.php");
 
-    $gabriel = new Usuario();
+//Carrega um usário
+//$gabriel = new Usuario();
+//$gabriel->loadbyId(4);
+//echo $root;
 
-    $gabriel->loadbyId(4);
+//Carrega uma lista usuáros
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-    echo $root;
+//Carrega uma lista de usuários buscando pleo login
+//$search = Usuario::search("Jhon");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Jhon", "1111");
+echo $usuario;
 
 ?>
